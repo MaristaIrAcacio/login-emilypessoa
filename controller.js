d83d49d873b4e54218e7d87a7c935496f94cbe60
Emily Pessoa <emymi2007@gmail.com>
2023-07-31 11:38:58 -0300

    alteração do js

--- controller.js
+++ controller.js
@@ -9,19 +9,27 @@ function acessar() {
     }
 }
 
-var dadoslista = [];
+// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
+
+var dadosLista = [];
 
 function salvarUser(){
     let nomeUser=document.getElementById("nomeUser").value;
 
     if(nomeUser){
-        dadoslista.push(nomeUser);
+        dadosLista.push(nomeUser);
+        criarLista();
     }
 }
 
+// FUNÇÃO PARA CRIAR LISTA DE USUÁRIO
+
 function criarLista(){
 let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-for(let i = 0;i <= (dadoslista.length - 1);i++){
-    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button>Editar</button>" 
+for(let i = 0;i <= (dadosLista.length - 1);i++){
+    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger'  onclick=''>Excluir</button></td></tr>";
+    document.getElementById('tabela').innerHTML = tabela;
+
+    }
 }
-}
+
